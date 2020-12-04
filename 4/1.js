9539ad7d8d9cd5e0405854d1404bd53c4f1035d5
Aiden Foxx <aiden.foxx.mail@gmail.com>
2020-12-04 19:31:14 +0100

    Optimized Day 4 regex

--- 4/1.js
+++ 4/1.js
@@ -1,5 +1,14 @@
 /**
  * Let's play golf!
  * NOTE: Only works with unix line endings.
+ * (?:
+ *   (?:cid:.+[\s\n]|)byr:.+[\s\n]|
+ *   (?:cid:.+[\s\n]|)iyr:.+[\s\n]|
+ *   (?:cid:.+[\s\n]|)eyr:.+[\s\n]|
+ *   (?:cid:.+[\s\n]|)hgt:.+[\s\n]|
+ *   (?:cid:.+[\s\n]|)hcl:.+[\s\n]|
+ *   (?:cid:.+[\s\n]|)ecl:.+[\s\n]|
+ *   (?:cid:.+[\s\n]|)pid:.+[\s\n]
+ * ){7}(?:cid:.+[\s\n]|)
  */
-console.log('Matches:', `\n${require('fs').readFileSync('data.txt', 'utf8')}`.match(/(?:(?:[\s\n]cid:.+|)[\s\n]byr:.+|(?:[\s\n]cid:.+|)[\s\n]iyr:.+|(?:[\s\n]cid:.+|)[\s\n]eyr:.+|(?:[\s\n]cid:.+|)[\s\n]hgt:.+|(?:[\s\n]cid:.+|)[\s\n]hcl:.+|(?:[\s\n]cid:.+|)[\s\n]ecl:.+|(?:[\s\n]cid:.+|)[\s\n]pid:.+){7}/gm).length);
+console.log('Matches:', `${require('fs').readFileSync('data.txt', 'utf8')}\n`.match(/(?:(?:cid:.+[\s\n]|)byr:.+[\s\n]|(?:cid:.+[\s\n]|)iyr:.+[\s\n]|(?:cid:.+[\s\n]|)eyr:.+[\s\n]|(?:cid:.+[\s\n]|)hgt:.+[\s\n]|(?:cid:.+[\s\n]|)hcl:.+[\s\n]|(?:cid:.+[\s\n]|)ecl:.+[\s\n]|(?:cid:.+[\s\n]|)pid:.+[\s\n]){7}(?:cid:.+[\s\n]|){7}(?:cid:.+[\s\n]|)/gm).length);
--- 4/2.js
+++ 4/2.js
@@ -2,13 +2,13 @@
  * Let's play golf!
  * NOTE: Only works with unix line endings.
  * (?:
- *   (?:[\s\n]cid:.+|)[\s\n]byr:(?:19[2-9]\d|200[0-2])|
- *   (?:[\s\n]cid:.+|)[\s\n]iyr:(?:201\d|2020)|
- *   (?:[\s\n]cid:.+|)[\s\n]eyr:(?:202\d|2030)|
- *   (?:[\s\n]cid:.+|)[\s\n]hgt:(?:(?:59|6\d|7[0-6])in|(?:1[5-8]\d|19[0-3])cm)|
- *   (?:[\s\n]cid:.+|)[\s\n]hcl:#[0-9a-f]{6}|
- *   (?:[\s\n]cid:.+|)[\s\n]ecl:(?:amb|blu|brn|gry|grn|hzl|oth)|
- *   (?:[\s\n]cid:.+|)[\s\n]pid:\d{9}
- * ){7}
+ *   (?:cid:.+[\s\n]|)byr:(?:19[2-9]\d|200[0-2])[\s\n]|
+ *   (?:cid:.+[\s\n]|)iyr:(?:201\d|2020)[\s\n]|
+ *   (?:cid:.+[\s\n]|)eyr:(?:202\d|2030)[\s\n]|
+ *   (?:cid:.+[\s\n]|)hgt:(?:(?:59|6\d|7[0-6])in|(?:1[5-8]\d|19[0-3])cm)[\s\n]|
+ *   (?:cid:.+[\s\n]|)hcl:#[0-9a-f]{6}[\s\n]|
+ *   (?:cid:.+[\s\n]|)ecl:(?:amb|blu|brn|gry|grn|hzl|oth)[\s\n]|
+ *   (?:cid:.+[\s\n]|)pid:\d{9}[\s\n]
+ * ){7}(?:cid:.+[\s\n]|)
  */
-console.log('Matches:', `\n${require('fs').readFileSync('data.txt', 'utf8')}`.match(/(?:(?:[\s\n]cid:.+|)[\s\n]byr:(?:19[2-9]\d|200[0-2])|(?:[\s\n]cid:.+|)[\s\n]iyr:(?:201\d|2020)|(?:[\s\n]cid:.+|)[\s\n]eyr:(?:202\d|2030)|(?:[\s\n]cid:.+|)[\s\n]hgt:(?:(?:59|6\d|7[0-6])in|(?:1[5-8]\d|19[0-3])cm)|(?:[\s\n]cid:.+|)[\s\n]hcl:#[0-9a-f]{6}|(?:[\s\n]cid:.+|)[\s\n]ecl:(?:amb|blu|brn|gry|grn|hzl|oth)|(?:[\s\n]cid:.+|)[\s\n]pid:\d{9}){7}/gm).length);
+console.log('Matches:', `${require('fs').readFileSync('data.txt', 'utf8')}\n`.match(/(?:(?:cid:.+[\s\n]|)byr:(?:19[2-9]\d|200[0-2])[\s\n]|(?:cid:.+[\s\n]|)iyr:(?:201\d|2020)[\s\n]|(?:cid:.+[\s\n]|)eyr:(?:202\d|2030)[\s\n]|(?:cid:.+[\s\n]|)hgt:(?:(?:59|6\d|7[0-6])in|(?:1[5-8]\d|19[0-3])cm)[\s\n]|(?:cid:.+[\s\n]|)hcl:#[0-9a-f]{6}[\s\n]|(?:cid:.+[\s\n]|)ecl:(?:amb|blu|brn|gry|grn|hzl|oth)[\s\n]|(?:cid:.+[\s\n]|)pid:\d{9}[\s\n]){7}(?:cid:.+[\s\n]|)/gm).length);
