1708c71a8fddeb5a93d4cfa07ff4c2d7869f7fe8
Aiden Foxx <aiden.foxx.mail@gmail.com>
2020-12-05 12:29:21 +0100

    Optimized Day 5

--- 5/1.js
+++ 5/1.js
@@ -1,18 +1,5 @@
-const getSeatId = (input, rowMin = 0, rowMax = 127, colMin = 0, colMax = 7) => {
-  if (rowMin === rowMax && colMin === colMax) {
-    return rowMin * 8 + colMin;
-  } else if (!input) {
-    return false;
-  }
-
-  const identifier = input.slice(0, 1);
-  return getSeatId(
-    input.slice(1),
-    identifier === 'B' ? Math.ceil((rowMin + rowMax) / 2) : rowMin,
-    identifier === 'F' ? Math.floor((rowMax + rowMin) / 2) : rowMax,
-    identifier === 'R' ? Math.ceil((colMin + colMax) / 2) : colMin,
-    identifier === 'L' ? Math.floor((colMax + colMin) / 2) : colMax
-  );
-};
-
-console.log('Max seat ID:', require('fs').readFileSync('data.txt', 'utf8').trim().split('\n').map((ticketId) => getSeatId(ticketId.trim())).sort((a, b) => a - b).pop());
+/**
+ * Let's play golf!
+ */
+const getSeatId = (input) => parseInt(input.slice(0,7).replace(/B/g, '1').replace(/F/g, '0'), 2) * 8 + parseInt(input.slice(7,10).replace(/R/g, '1').replace(/L/g, '0'), 2);
+console.log('Max seatId:', require('fs').readFileSync('data.txt', 'utf8').trim().split('\n').map((ticketId) => getSeatId(ticketId.trim())).sort((a, b) => a - b).pop());
--- 5/2.js
+++ 5/2.js
@@ -1,26 +1,12 @@
-const getSeatId = (input, rowMin = 0, rowMax = 127, colMin = 0, colMax = 7) => {
-  if (rowMin === rowMax && colMin === colMax) {
-    return rowMin * 8 + colMin;
-  } else if (!input) {
-    return false;
-  }
-
-  const identifier = input.slice(0, 1);
-  return getSeatId(
-    input.slice(1),
-    identifier === 'B' ? Math.ceil((rowMin + rowMax) / 2) : rowMin,
-    identifier === 'F' ? Math.floor((rowMax + rowMin) / 2) : rowMax,
-    identifier === 'R' ? Math.ceil((colMin + colMax) / 2) : colMin,
-    identifier === 'L' ? Math.floor((colMax + colMin) / 2) : colMax
-  );
-};
+/**
+ * Let's play golf!
+ */
+const getSeatId = (input) => parseInt(input.slice(0,7).replace(/B/g, '1').replace(/F/g, '0'), 2) * 8 + parseInt(input.slice(7,10).replace(/R/g, '1').replace(/L/g, '0'), 2);
 
 let prevVal = -1;
-
 require('fs').readFileSync('data.txt', 'utf8').trim().split('\n').map((ticketId) => getSeatId(ticketId.trim())).sort((a, b) => a - b).forEach((seatId) => {
   if (prevVal !== -1 && seatId - prevVal > 1) {
     console.log('Undefined seatId(s) between:', prevVal, seatId);
   }
-
   prevVal = seatId;
 });
