f724869a20bf19bc206203f5df314af52cb259fb
Aiden Foxx <aiden.foxx.mail@gmail.com>
2020-12-05 12:05:05 +0100

    Day 5

--- 4/1.js
+++ 4/1.js
@@ -9,6 +9,6 @@
  *   (?:cid:.+[\s\n]|)hcl:.+[\s\n]|
  *   (?:cid:.+[\s\n]|)ecl:.+[\s\n]|
  *   (?:cid:.+[\s\n]|)pid:.+[\s\n]
- * ){7}(?:cid:.+[\s\n]|)
+ * ){7}(?:cid:.+[\n]|)
  */
-console.log('Matches:', `${require('fs').readFileSync('data.txt', 'utf8')}\n`.match(/(?:(?:cid:.+[\s\n]|)byr:.+[\s\n]|(?:cid:.+[\s\n]|)iyr:.+[\s\n]|(?:cid:.+[\s\n]|)eyr:.+[\s\n]|(?:cid:.+[\s\n]|)hgt:.+[\s\n]|(?:cid:.+[\s\n]|)hcl:.+[\s\n]|(?:cid:.+[\s\n]|)ecl:.+[\s\n]|(?:cid:.+[\s\n]|)pid:.+[\s\n]){7}(?:cid:.+[\s\n]|){7}(?:cid:.+[\s\n]|)/gm).length);
+console.log('Matches:', `${require('fs').readFileSync('data.txt', 'utf8')}\n`.match(/(?:(?:cid:.+[\s\n]|)byr:.+[\s\n]|(?:cid:.+[\s\n]|)iyr:.+[\s\n]|(?:cid:.+[\s\n]|)eyr:.+[\s\n]|(?:cid:.+[\s\n]|)hgt:.+[\s\n]|(?:cid:.+[\s\n]|)hcl:.+[\s\n]|(?:cid:.+[\s\n]|)ecl:.+[\s\n]|(?:cid:.+[\s\n]|)pid:.+[\s\n]){7}(?:cid:.+[\s\n]|){7}(?:cid:.+[\n]|)/gm).length);
---
+++ 5/1.js
@@ -0,0 +1,18 @@
+const getSeatId = (input, rowMin = 0, rowMax = 127, colMin = 0, colMax = 7) => {
+  if (rowMin === rowMax && colMin === colMax) {
+    return rowMin * 8 + colMin;
+  } else if (!input) {
+    return false;
+  }
+
+  const identifier = input.slice(0, 1);
+  return getSeatId(
+    input.slice(1),
+    identifier === 'B' ? Math.ceil((rowMin + rowMax) / 2) : rowMin,
+    identifier === 'F' ? Math.floor((rowMax + rowMin) / 2) : rowMax,
+    identifier === 'R' ? Math.ceil((colMin + colMax) / 2) : colMin,
+    identifier === 'L' ? Math.floor((colMax + colMin) / 2) : colMax
+  );
+};
+
+console.log('Max seat ID:', require('fs').readFileSync('data.txt', 'utf8').trim().split('\n').map((ticketId) => getSeatId(ticketId.trim())).sort((a, b) => a - b).pop());
