5bca4f3e093d1c4ac8c054a93403be0cc24b8d35
Aiden Foxx <aiden.foxx.mail@gmail.com>
2020-12-14 16:59:37 +0100

    Day 13 optimization and simple multithreading

--- 13/1.js
+++ 13/1.js
@@ -1,4 +1,4 @@
-const data = require('fs').readFileSync('data.txt', 'utf8').trim().split('\n');
+const data = require('fs').readFileSync('data.txt', 'utf8').split('\n');
 const currentTime = Number(data[0]);
 const closestBus = data[1].split(',').map((id) => id !== 'x' ? [Number(id), currentTime - (currentTime % Number(id)) + Number(id)] : null).filter(Boolean).sort((a, b) => a[1] - b[1])[0];
 
